5d9b0ed1f31aa79776753b82198f7173d4a14383
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-04-03 09:47:58 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const submitData = {
+    userName: "Becky",
+    userEmail:"rebeccanakhanu@gmail.com"
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("hello, something went wrong");
+      console.log(error.message);
+    });
